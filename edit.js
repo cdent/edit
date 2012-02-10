5e605ae34196a7893afa09004aa92a0511673205
Chris Dent <chris.dent@gmail.com>
2012-02-10 20:50:25 +0000

    Enable most of the main features
    * saving
    * reversion
    * tag parsing
    * recent changes updating
    * use of #hash to load a tiddler...
    * ...or load by clicking tiddler in sidebar
    * background storing and reuse of tiddler.fields (so we don't
      clobber on save)
    
    Other things to do:
    
    * click to view html rep from changes
    * a way to go home
    * make backstage work properly
    * portability
    * save in progress indicator in ui
    * tranform from yucky javascript to good javascript

--- edit.js
+++ edit.js
@@ -3,9 +3,47 @@ var Set = function() {}
 Set.prototype.add = function(o) { this[o] = true; }
 Set.prototype.remove = function(o) { delete this[o]; }
 
-var recentTags = new Set();
+var recentTags = new Set()
+    , currentFields = {}
+    , host
+    , space;
+
+$('#revert').bind('click', function() {
+    startEdit($('#editor > h1').text());
+});
+
+$('#save').bind('click', function() {
+    saveEdit();
+});
+
+function saveEdit() {
+    var title = $('#editor > h1').text();
+    var tagString = $('input[name=tags]').val();
+    var text = $('textarea[name=text]').val();
+    var tags = [];
+    var matches = tagString.match(/([^ \]\[]+)|(?:\[\[([^\]]+)\]\])/g) || [];
+    $.each(matches, function(index, value) {
+        tags.push(value.replace(/[\]\[]+/g, ''));
+    });
+    var tiddler = {};
+    tiddler.text = text;
+    tiddler.tags = tags;
+    tiddler.fields = currentFields;
+    var jsonText = JSON.stringify(tiddler);
+    $.ajax({
+        url: host + '/bags/' + encodeURIComponent(space) + '_public'
+            + '/tiddlers/' + encodeURIComponent(title),
+        type: "PUT",
+        contentType: 'application/json',
+        data: jsonText,
+        success: function() {
+            changes();
+        }
+    });
+}
 
 function updateTags(tags) {
+    $('#tags').empty();
     $.each(tags, function(tag, value) {
         if (typeof value === 'boolean') {
             var taglink = $('<a>')
@@ -13,6 +51,9 @@ function updateTags(tags) {
                 .addClass('taglink')
                 .bind('click', function() {
                     var text = $(this).text();
+                    if (/ /.test(text)) {
+                        text = '[[' + text + ']]';
+                    }
                     $('#editor input').val(function(index, value) {
                         return value + ' ' + text;
                     });
@@ -22,20 +63,58 @@ function updateTags(tags) {
     });
 }
 
-function changes() {
+function startEdit(tiddlerTitle) {
+    window.location.hash = tiddlerTitle;
+    $('#editor > h1').text(tiddlerTitle);
     $.ajax({
         dataType: 'json',
-        url: 'http://cdent.tiddlyspace.com/bags/cdent_public/tiddlers?sort=-modified;limit=20',
+        url: host + '/' + encodeURIComponent(tiddlerTitle),
+        success: function(tiddler) {
+            $('textarea[name=text]').val(tiddler.text);
+            var tagList = [];
+            currentFields = tiddler.fields;
+            $.each(tiddler.tags, function(index, value) {
+                if (value.match(/ /)) {
+                    tagList.push('[[' + value + ']]');
+                } else {
+                    tagList.push(value);
+                }
+            });
+            $('input[name=tags]').val(tagList.join(' '));
+        }
+    });
+}
+
+function checkHash() {
+    var hash = window.location.hash;
+    if (hash) {
+        hash = hash.replace(/^#/, '');
+        startEdit(hash);
+    }
+}
+
+function changes() {
+    $('#recents > ul').empty();
+    $.ajax({
+        dataType: 'json',
+        url: host + '/tiddlers?select=tag:!excludeLists;sort=-modified;limit=20',
         success: function(tiddlers) {
             $.each(tiddlers, function(index, tiddler) {
                 $.each(tiddler.tags, function(index, tag) {
                     recentTags.add(tag);
                 })
-                $('#recents > ul').append('<li>' + tiddler.title + '</li>');
+                var penSpan = $('<span>').text('\u270E')
+                    .bind('click', function() {
+                        startEdit($(this).parent().attr('data-tiddler-title'));
+                    });
+                var list = $('<li>').attr('data-tiddler-title',
+                    tiddler.title).text(tiddler.title).prepend(penSpan);
+                $('#recents > ul').append(list);
             });
             updateTags(recentTags);
         }
     });
+    checkHash();
 }
 
 function init() {
@@ -44,9 +123,17 @@ function init() {
                         xhr.setRequestHeader("X-ControlView", "false");
                     }
     });
-    changes();
+    $.ajax({
+        dataType: 'json',
+        url: 'http://cdent.tiddlyspace.com/status',
+        success: function(data) {
+            space = data.space.name;
+            host = data.server_host.scheme + '://'
+                + space + '.' + data.server_host.host;
+            changes();
+        }
+    });
 }
-
 
 
 $(init);
